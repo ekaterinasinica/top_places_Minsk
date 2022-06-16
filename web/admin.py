8e3756c365e0d68d21f4e4503beecfb0a76bc224
ekaterinasinica <105008581+ekaterinasinica@users.noreply.github.com>
2022-06-16 15:01:02 +0300

    add model Card

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import Card
+
+admin.site.register(Card)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="img/")
+
+
+    def __str__(self):
+        return f'{self.title}'
